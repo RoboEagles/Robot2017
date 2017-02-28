f6b27d549603d83eb78a25179fa840220ef78eba
Jon Wallen <jdwallen@hotmail.com>
2017-02-28 11:31:08 -0800

    Improve filter efficiency.  Add avg and std dev methods.

--- src/org/usfirst/frc4579/filters/AverageFilter.java
+++ src/org/usfirst/frc4579/filters/AverageFilter.java
@@ -1,56 +1,85 @@
-package org.usfirst.frc4579.filters;
+package org.usfirst.frc.team4579.utilities;
+
+/*************************************************************************************
+ * AVERAGE STATS
+ * 
+ * Allows the caller to maintain a running average of sample values.  Standard deviation
+ * is also available.  The class is instantiated with the max number of samples that
+ * should be averaged.
+ *
+ *************************************************************************************/
 
 public class AverageFilter {
 	
-	private int arrayCount;
-	private double[] averageArray;
+	private final int arraySize;      // Number of elements in averageArray.
+	private int       newest = -1;    // Array index of the newest entry in the array.
+	private int       numEntries = 0; // Number of entries used in the array.
+	private double    sum = 0.0;      // Current sum of array entries.
+	private double[]  averageArray;   // The array that contains the sample values.
 	
-	public AverageFilter(int arrayCount) {
-		this.arrayCount = arrayCount;
-		averageArray = new double[arrayCount];		
-		
-		fillArray();
+	//*************************************************************************************
+	// Constructor.
+	//*************************************************************************************
+	public AverageFilter(int numberOfSamples) {
+		this.arraySize    = numberOfSamples;
+		this.averageArray = new double[numberOfSamples];		
 	}
 	
+	//*************************************************************************************
+	// Add "input" into the running average without returning the average (filtered value).
+	//*************************************************************************************
+	public void accumulate (double input) {
+		
+		// Add the new value into the running sum.
+		sum += input;
+		
+		// Compute the array index of where to put the newest input.
+		newest = (newest+1) % arraySize;
+		
+		// if the array is full
+		if (numEntries == arraySize) {
+			
+			// "newest" has wrapped around and now points to the oldest entry. 
+			// Remove the oldest entry from the sum.
+			sum -= averageArray[newest];
+			
+		}
+		else
+			// Increment the number of samples in the array.
+			numEntries++;
+
+		// Save the newest value.
+		averageArray[newest] = input;
+	}
+	
+	//*************************************************************************************
+	// Add "input" into the running average and return the average (filtered value).
+	//*************************************************************************************
 	public double filter(double input) {
-		double output = 0;
 		
-		this.averageArray = shiftArray(input);
-		
-		output = arraySum() / arrayCount;
-		
-		
-		return output;
+		accumulate(input);
+
+		return average();
 	}
 	
-	private double[] shiftArray(double newValue) {
-		double[] output = new double[arrayCount];
-		
-		for(int i = 0; i < (averageArray.length - 1); i++){
-			output[i] = averageArray[i+1];
-		}
-		
-		output[averageArray.length - 1] = newValue;
-		
-		return output;
-	}
+	//*************************************************************************************
+	// Return the current running average.
+	//*************************************************************************************
+	public double average () { return sum / (double)numEntries; }
 	
-	private double arraySum() {
-		double sum = 0;
-		for(int i = 0; i<(averageArray.length); i++) {
-			sum += averageArray[i];
-		}
-		return sum;
-	}
-	
-	private void fillArray(double fillValue) {
-		for(int i = 0; i < averageArray.length; i++) {
-			averageArray[i] = fillValue;
-		}
-	}
-	
-	private void fillArray() {
-		this.fillArray(0);
+	//*************************************************************************************
+	// Return the standard deviation of the current running average.
+	//*************************************************************************************
+	public double stdDeviation () { 
+		
+		double avg    = average();
+		double stdDev = 0.0;
+		
+		for (int i=0; i < numEntries; i++) stdDev += Math.pow((averageArray[i] - avg), 2.0);
+		
+		stdDev = Math.sqrt(stdDev / (double)numEntries);
+		
+		return stdDev; 
 	}
 
 }
