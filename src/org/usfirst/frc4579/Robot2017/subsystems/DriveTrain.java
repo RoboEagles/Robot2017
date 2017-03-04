58174f2734aa22868d7f9ab9a0cc09ae2a9ceca5
IvanDiakonu <i_diakonu@hotmail.com>
2017-03-03 16:52:50 -0800

    Added drivestraight updates

--- src/org/usfirst/frc4579/Robot2017/subsystems/DriveTrain.java
+++ src/org/usfirst/frc4579/Robot2017/subsystems/DriveTrain.java
@@ -136,6 +136,9 @@ public class DriveTrain extends PIDSubsystem {
     public void driveStraight() {
     	// To get the robot to drive straight, compute a correction based on z gyro angle rate and z angle.
 	    // Apply half the correction to each wheel to make the middle of the robot move at the desired speed.
+   //This equation is for the backup robot.
+	    //double halfCorrection = ((Robot.meas.getAngleRate() * .01) + (Robot.meas.getAngle() * .02)) /2.0;
+   //This equation is for the competition robot.
 	    double halfCorrection = ((Robot.meas.getAngleRate() * .006) + (Robot.meas.getAngle() * .016)) /2.0;
 	    Robot.driveTrain.driveEachMotor(0.2 + halfCorrection, 0.2 - halfCorrection);
     }
